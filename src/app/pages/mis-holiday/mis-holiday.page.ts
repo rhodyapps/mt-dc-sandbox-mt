58b1dee3afc8f65772fe6c2f36df93fd370175b9
tnewman <tnewman@meditech.com>
2020-12-25 06:20:45 -0500

    resolve detail routing bug

--- src/app/pages/mis-holiday/mis-holiday.page.ts
+++ src/app/pages/mis-holiday/mis-holiday.page.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { switchMap, map, shareReplay } from 'rxjs/operators';
-import {DbService } from '../../services/db.service';
-
-import { ModalController } from '@ionic/angular';
-import { AuthService } from '../../services/auth.service';
-
-
 
 @Component({
   selector: 'app-mis-holiday',
