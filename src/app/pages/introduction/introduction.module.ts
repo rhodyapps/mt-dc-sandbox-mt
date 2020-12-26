ba39c15d57dd7faa5a3465f61c1ff8126a7f6792
tnewman <tnewman@meditech.com>
2020-12-26 05:45:46 -0500

    sign in complete

--- src/app/pages/introduction/introduction.module.ts
+++ src/app/pages/introduction/introduction.module.ts
@@ -4,15 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { IntroductionPageRoutingModule } from './introduction-routing.module';
 import { IntroductionPage } from './introduction.page';
-import { SignupPageModule } from '../signup/signup.module';
-
+// import { SignupPageModule } from '../signup/signup.module';
+import { SharedComponentsModule } from '../../components/shared-components.module';
+import { LoginPageModule } from '../../pages/login/login.module';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     IntroductionPageRoutingModule,
-    SignupPageModule
+    SharedComponentsModule,
+    LoginPageModule
+   // SignupPageModule
   ],
   declarations: [IntroductionPage]
 })
