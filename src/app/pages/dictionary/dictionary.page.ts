6aebf0da9d70477c4f5c350d08c4cdd27a3e55f8
rhodyapps <rhodyapps@gmail.com>
2021-01-15 18:28:36 -0500

    Firestore svc with dictionary component

--- src/app/pages/dictionary/dictionary.page.ts
+++ src/app/pages/dictionary/dictionary.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Transaction } from '../../models/transaction';
+import { FirestoreService } from '../../services/firestore.service';
 
 @Component({
   selector: 'app-dictionary',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DictionaryPage implements OnInit {
 
-  constructor() { }
+  ref: AngularFirestoreCollection<Transaction>;
+  transactions: Observable<Transaction[]>;
+
+  constructor(public db: FirestoreService, private afs: AngularFirestore) { }
 
   ngOnInit() {
+   // this.ref = this.afs.collection('transactions')
+    // this.transactions = this.ref.valueChanges()
+     this.transactions = this.db.col$('transactions');
   }
 
 }
